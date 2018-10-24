fb3b38ae4fa485f3f636331003e2c3587fecf9ed
TGM - Thiago Mota <thiago.mota@totvs.com.br>
2018-10-23 23:29:25 -0300

    Separa Todo

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,7 @@
 
       <v-content>
         <Ponto></Ponto>
-
-        <h2>TODO:</h2>
-        <ul>
-          <li>Calcular total de horas</li>
-          <li>Converter para hora decimal</li>
-          <li>Criar DashBoard com os totais</li>
-          <li>Criar Rotas e arrumar menu</li>
-          <li>Criar componente e coleção para registro de OS</li>
-          <li>Host</li>
-          <li>App com react native</li>
-          <li>Coleção e componente para previsão da folha</li>
-        </ul>
+        <Todo></Todo>
 
       </v-content>
 
@@ -28,6 +17,7 @@
 import AppHeader from './components/template/AppHeader'
 import AppFooter from './components/template/AppFooter'
 import Ponto from './components/ponto/Ponto'
+import Todo from './components/Todo'
 
 import store from './store'
 
