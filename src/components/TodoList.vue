6a456d7a9f3c7e03a99424953dfb3b66ee32560b
TGM - Thiago Mota <thiago.mota@totvs.com.br>
2018-10-25 01:59:16 -0300

    Calculo de horas

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,12 +5,13 @@
             <li>Calcular total de horas</li>
             <li>Converter para hora decimal</li>
             <li>Criar DashBoard com os totais</li>
+            <li>Vue testes () </li>
             <li>Criar Rotas e arrumar menu</li>
             <li>Criar componente e coleção para registro de OS</li>
-            <li>Host</li>
             <li>App com react native</li>
             <li>Coleção e componente para previsão da folha</li>
             <li>Ajustar campo hora do Registro ponto</li>
+            <li>Criar coleção para configurações, adicionar a tolerancia de 10 minutos para o ponto</li>
             <li>Melhorar visual</li>
         </ul>
     </div>
