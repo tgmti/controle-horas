57aa6028b7d35eb19c70b509afd1631c292b72d5
TGM - Thiago Mota <thiago.mota@totvs.com.br>
2018-10-25 02:00:07 -0300

    Calculo de horas

---
+++ src/util/horas.js
@@ -0,0 +1,12 @@
+
+export function difHorStr(cHorIni, cHorFim) {
+    // StrHora das duas
+    // Diferença * 24
+}
+
+export function HoraToFloat(cHora) {
+    // Valida hora
+    // Pega primeira parte
+    // Soma a com (segunda parte e divide por 60)
+    // Truncar as casas decimais???
+}
