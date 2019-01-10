0ecb98da9cb8925be7bc3a5bacdc8061beadb17c
TGM - Thiago Mota <thiago.mota@totvs.com.br>
2019-01-10 01:06:39 -0200

    Modelo de inclusão com Formulário

--- src/components/template/RegistraHora.vue
+++ src/components/template/RegistraHora.vue
@@ -1,6 +1,58 @@
 <template>
 
-  <v-form persistent>
+    <div>
+        
+    <v-toolbar flat>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+      <v-divider
+        class="mx-2"
+        inset
+        vertical
+      ></v-divider>
+
+      <v-spacer></v-spacer>
+
+      <v-dialog v-model="dialog" max-width="80%">
+        <v-btn slot="activator" color="primary" dark class="mb-2">Novo</v-btn>
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ title }}</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Dessert name"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Calories"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Fat (g)"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Carbs (g)"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Protein (g)"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" flat @click="close">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+    </v-toolbar>
+
+
+  <v-form>
     <v-container>
       <v-layout row wrap>
 
@@ -42,6 +94,10 @@
     </v-container>
   </v-form>
   
+
+    </div>
+
+
 </template>
 
 <script>
@@ -49,6 +105,7 @@
 export default {
     name: "RegistraHora",
     props: {
+        title: { type: String, required: true, default: 'Registro de Horas' },
         headers: { type: Array, required: true },
         defaultItem: { type: Function, required: true },
         fnCommit: { type: Function, required: false },
@@ -59,7 +116,13 @@ export default {
     data () {
         return {
             menu:false,
+            dialog: false
         }
+    },
+    watch: {
+      dialog (val) {
+        val || this.close()
+      }
     },
     created () {
         this.resetForm()
@@ -67,17 +130,22 @@ export default {
     methods: {
         commitReg () {
             if ( this.fnCommit ) {
-                this.fnCommit(this.editedItem).then(
-                    this.resetForm()
+                this.fnCommit(this.editedItem).then(() => {
+                    this.dialog = false
+                    this.resetForm()                    
+                }
                 )
             }
         },
         resetForm () {
             this.defaultItem()
         },
+        close () {
+            this.dialog = false
+        },
         classDef (type) {
-            if ( type === 'date') return ['xs12','sm2'];
-            if ( type === 'time') return ['xs6','sm1'];
+            if ( type === 'date') return ['xs12','sm2','md2'];
+            if ( type === 'time') return ['xs6','sm1','md1'];
             return ['xs12','sm2'];
         }
     }
